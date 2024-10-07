28ad06162abdaf31e10c8d8b5e4ca3a0155e9043
Amine Dahbi <aminedahbi2016@hotmail.com>
2024-10-07 09:56:06 +0200

    Add Tailwind CSS configuration file and TypeScript configuration file

---
+++ ecommerce-app/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        // Adding custom spacing
+        '36': '9rem',
+        '90': '90px',
+      },
+    },
+  },
+  plugins: [],
+}
