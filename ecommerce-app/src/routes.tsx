a51ed6636848066a816c7a7e6fbe1c0625eb8a98
Amine Dahbi <aminedahbi2016@hotmail.com>
2024-10-07 09:55:23 +0200

    Add routes

---
+++ ecommerce-app/src/routes.tsx
@@ -0,0 +1,22 @@
+import {Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductDetails from './pages/ProductPage';
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import SignInPage from './pages/SignInPage';
+
+function RouteContainer() {
+  return (
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/signIn" element={<SignInPage />} />
+        {/* <Route path="/product/:id" element={<ProductDetails product={{ name: '', description: '', price: 0, category: '', availability: '' }} />} /> */}
+        {/* <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} /> */}
+      </Routes>
+  );
+}
+
+export default RouteContainer;
